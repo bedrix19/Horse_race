a31d85da38c42d5f07cfd41ec4c13657b4f8d2ee
bedrix19 <146009176+bedrix19@users.noreply.github.com>
2024-07-31 12:33:27 +0200

    Update horse_race.py

--- horse_race.py
+++ horse_race.py
@@ -1,4 +1,7 @@
 import os
+import time
+import sys
+
 clear = lambda: os.system('clear')
 clear()
 print('Cleared')
@@ -8,3 +11,18 @@ print('''
 ' / / --`--,   
  /  \    | '   
 ''')
+
+def Main():
+    print('Welcome to the horse race')
+
+Main()
+animation = "|/-\\"
+start_time = time.time()
+while True:
+    for i in range(4):
+        time.sleep(0.2)  # Feel free to experiment with the speed here
+        sys.stdout.write("\r" + animation[i % len(animation)])
+        sys.stdout.flush()
+    if time.time() - start_time > 10:  # The animation will last for 10 seconds
+        break
+sys.stdout.write("\rDone!")
