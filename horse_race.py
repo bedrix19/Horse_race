08ca2f6d3b8486f88cf0a8e81b23f049fae19116
bedrix19 <146009176+bedrix19@users.noreply.github.com>
2024-07-31 14:50:52 +0200

    Update horse_race.py

--- horse_race.py
+++ horse_race.py
@@ -2,20 +2,36 @@ import os
 import time
 import sys
 
-clear = lambda: os.system('clear')
-clear()
-print('Cleared')
-print('''
-   _____,,;;;`;
-,~(  )  , )~~\|
-' / / --`--,   
- /  \    | '   
-''')
+from enum import Enum
+
+class Horse(Enum):
+    ALBERT  = 'A'
+    BELLA   = 'B'
+    CASH    = 'C'
+
 
 def Main():
-    print('Welcome to the horse race')
+    print('####################### Welcome to the horse race #######################')
+    print('Choose your horse:')
+    print([h.value for h in Horse])
 
 Main()
+
+#clear = lambda: os.system('clear')
+#clear()
+#print('Cleared')
+#print('''
+#   _____,,;;;`;
+#,~(  )  , )~~\|
+#' / / --`--,   
+# /  \    | '   
+#''')
+
+#         -~~,
+#  ,; _ _~ |\|
+# ;; ( )_, )
+# ;; /|  |.\
+
 animation = "|/-\\"
 start_time = time.time()
 while True:
@@ -23,6 +39,6 @@ while True:
         time.sleep(0.2)  # Feel free to experiment with the speed here
         sys.stdout.write("\r" + animation[i % len(animation)])
         sys.stdout.flush()
-    if time.time() - start_time > 10:  # The animation will last for 10 seconds
+    if time.time() - start_time > 5:  # The animation will last for 5 seconds
         break
-sys.stdout.write("\rDone!")
+sys.stdout.write("\rDone!\n")
