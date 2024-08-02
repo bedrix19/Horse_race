3a4beca35b7752b653ad835c39a2a804a2a98013
bedrix19 <146009176+bedrix19@users.noreply.github.com>
2024-08-02 13:37:51 +0200

    Update horse_race.py

--- horse_race.py
+++ horse_race.py
@@ -1,6 +1,7 @@
 import os
 import time
 import sys
+import threading
 
 from enum import Enum
 
@@ -9,11 +10,41 @@ class Horse(Enum):
     BELLA   = 'B'
     CASH    = 'C'
 
+class Loader:
+    def __init__(self):
+        self.stop_loading = False
+        self.load_thread = threading.Thread(target=self.loader)
+
+    def loader(self):
+        animation = "|/-\\"
+        while not self.stop_loading :
+            for i in range(4):
+                time.sleep(0.2)  # Feel free to experiment with the speed here
+                print(f"\r{animation[i % len(animation)]}",end='',flush=True)
+
+    def start(self):
+        self.load_thread.start()
+
+    def stop(self):
+        self.stop_loading = True
+        self.load_thread.join()
+        print('\r'+' '*20+'\r',end='')
+
 
 def Main():
+    loader = Loader()
     print('####################### Welcome to the horse race #######################')
     print('Choose your horse:')
-    print([h.value for h in Horse])
+    [print(x) for x in [f"[{str(h.value)}] - {str(h.name)}" for h in Horse]]
+
+    loader.start()
+    option = input()
+    loader.stop()
+
+    match option.upper():
+        case 'A':
+            print('@')
+    print('You choose:',option)
 
 Main()
 
@@ -31,7 +62,7 @@ Main()
 #  ,; _ _~ |\|
 # ;; ( )_, )
 # ;; /|  |.\
-
+'''
 animation = "|/-\\"
 start_time = time.time()
 while True:
@@ -42,3 +73,4 @@ while True:
     if time.time() - start_time > 5:  # The animation will last for 5 seconds
         break
 sys.stdout.write("\rDone!\n")
+'''
